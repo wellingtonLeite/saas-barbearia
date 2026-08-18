466a0a16678ec2926f3618ad5180b803843f4dd0
Wellington Leite <wellington.leite@criativamarketing.com>
2026-08-18 16:51:07 -0300

    fix: remove temporary build-breaking files

--- check-plans.ts
+++
@@ -1,1 +0,0 @@
-import { PrismaClient } from './src/generated/prisma/client/index.js'; const prisma = new PrismaClient(); async function main() { console.log(await prisma.plan.findMany()); } main().finally(() = 
